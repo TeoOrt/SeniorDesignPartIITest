9b3bd76e32cb6abb81e0b9c9c3e0ca9f72244c2a
Mateo Ortega <mateo041ortega@gmail.com>
2022-08-22 22:01:36 -0600

    Adding files for setup

---
+++ getinfo.py
@@ -0,0 +1,7 @@
+##get information off from the Esp32
+##How am I doing that IDK, maybe esp32 writes into a file on windows and 
+#then my python parsers it but maybe that too complicated because it might be binary.
+
+class GetMyInfo:
+    def __init__(self):
+        print('Hello World')
---
+++ sendinfoback.py
@@ -0,0 +1,6 @@
+##in this file we are sending data back to ESP
+#Most likely in EUSCII to make things easier
+
+class sendinfoback:
+    def __init__(self):
+        print('Hello back!!!!!!!!')
